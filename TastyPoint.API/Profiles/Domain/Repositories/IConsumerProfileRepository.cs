1c10fcb2dcbeb01a020e209c57e4c078a9c6dc90
JostyTafur <89022172+JostyTafur@users.noreply.github.com>
2022-11-08 15:37:45 -0500

    feat(Profile): implemented profile repositories

--- TastyPoint.API/Profiles/Domain/Repositories/IConsumerProfileRepository.cs
+++ TastyPoint.API/Profiles/Domain/Repositories/IConsumerProfileRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace TastyPoint.API.Profiles.Domain.Repositories;
+﻿using TastyPoint.API.Profiles.Domain.Models;
+
+namespace TastyPoint.API.Profiles.Domain.Repositories;
 
 public interface IConsumerProfileRepository
 {
-    
+    Task<IEnumerable<ConsumerProfile>> ListAsync();
+    Task AddAsync(ConsumerProfile consumerProfile);
+    Task<ConsumerProfile> FindByIdAsync(int id);
+    void Update(ConsumerProfile consumerProfile);
+    void Remove(ConsumerProfile consumerProfile);
 }
