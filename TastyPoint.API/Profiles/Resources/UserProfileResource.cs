fd0d5ec75b769174349719a324b751e23d5acc1a
JostyTafur <89022172+JostyTafur@users.noreply.github.com>
2022-11-10 14:47:35 -0500

    chore: added user profile resource and mapping

---
+++ TastyPoint.API/Profiles/Resources/UserProfileResource.cs
@@ -0,0 +1,13 @@
+﻿namespace TastyPoint.API.Profiles.Resources;
+
+public class UserProfileResource
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Type { get; set; }
+    
+    //Relationships
+    //public UserResource User {get; set;}
+    //public FoodStoreResource FoodStore {get;set;}
+}
