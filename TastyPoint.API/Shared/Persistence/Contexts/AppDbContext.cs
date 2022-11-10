c63d5c6fe779ae8f703430295b6aeeb0b8112776
Jesica Jaramillo A <81380838+Rut163@users.noreply.github.com>
2022-11-10 14:53:35 -0500

    feat(Subscription): implemented BusinessPlanRepository

--- TastyPoint.API/Shared/Persistence/Contexts/AppDbContext.cs
+++ TastyPoint.API/Shared/Persistence/Contexts/AppDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using TastyPoint.API.Selling.Domain.Models;
 using TastyPoint.API.Shared.Extensions;
+using TastyPoint.API.Subscription.Domain.Models;
 
 namespace TastyPoint.API.Shared.Persistence.Contexts;
 
@@ -9,6 +10,7 @@ public class AppDbContext: DbContext
 {
     public DbSet<Pack> Packs { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<BusinessPlan> BusinessPlans { get; set; }
     public AppDbContext(DbContextOptions options) : base(options)
     {
     }
@@ -29,6 +31,14 @@ public class AppDbContext: DbContext
         builder.Entity<Pack>().HasKey(p => p.Id);
         builder.Entity<Pack>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
         builder.Entity<Pack>().Property(p => p.Name).IsRequired().HasMaxLength(100);
+        
+        //Pack BusinessPlans Mapping Configuration
+        builder.Entity<BusinessPlan>().ToTable("BusinessPlans");
+        builder.Entity<BusinessPlan>().HasKey(p => p.Id);
+        builder.Entity<BusinessPlan>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<BusinessPlan>().Property(p => p.CurrentPlan).IsRequired().HasMaxLength(100);
+        builder.Entity<BusinessPlan>().Property(p => p.Description).IsRequired().HasMaxLength(500);
+        builder.Entity<BusinessPlan>().Property(p => p.PlanPrice).IsRequired();
 
         //Relationships
         builder.Entity<Pack>()
