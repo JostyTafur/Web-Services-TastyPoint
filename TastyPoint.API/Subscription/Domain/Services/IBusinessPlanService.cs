52cc4279421bda05e8f2de7389cd2edfbcbb4471
Jesica Jaramillo A <81380838+Rut163@users.noreply.github.com>
2022-11-10 14:23:10 -0500

    feat(Subscription): added BusinessPlan Domain

---
+++ TastyPoint.API/Subscription/Domain/Services/IBusinessPlanService.cs
@@ -0,0 +1,13 @@
+﻿using TastyPoint.API.Subscription.Domain.Models;
+using TastyPoint.API.Subscription.Domain.Services.Communication;
+
+namespace TastyPoint.API.Subscription.Domain.Services;
+
+public interface IBusinessPlanService
+{
+    Task<IEnumerable<BusinessPlan>> ListAsync();
+    Task<BusinessPlanResponse> FindByIdAsync(int businessPlanId);
+    Task<BusinessPlanResponse> SaveAsync(BusinessPlan businessPlan);
+    Task<BusinessPlanResponse> UpdateAsync(int promotionId, BusinessPlan businessPlan);
+    Task<BusinessPlanResponse> DeleteAsync(int id);
+}
