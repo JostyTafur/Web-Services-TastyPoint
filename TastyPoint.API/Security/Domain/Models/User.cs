2b3021433bcf73f7cb60d22e71152af61648274f
David-Torres-V <u202010942@upc.edu.pe>
2022-11-15 15:52:02 -0500

    feat fix security

--- TastyPoint.API/Security/Domain/Models/User.cs
+++ TastyPoint.API/Security/Domain/Models/User.cs
@@ -6,7 +6,6 @@ public class User
     public int Id { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
-    public string Password { get; set; }
     
     [JsonIgnore]
     public string PasswordHash { get; set; }
