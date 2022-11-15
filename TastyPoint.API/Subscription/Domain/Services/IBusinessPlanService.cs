ed45d4e62ffb0865ba0c004efc066be6922a0115
Jesica Jaramillo A <81380838+Rut163@users.noreply.github.com>
2022-11-15 14:47:51 -0500

    feat(Subscription): implemented BusinessPlanResource, BusinessPlanService and BusinessPlanController.

--- TastyPoint.API/Subscription/Domain/Services/IBusinessPlanService.cs
+++ TastyPoint.API/Subscription/Domain/Services/IBusinessPlanService.cs
@@ -8,6 +8,6 @@ public interface IBusinessPlanService
     Task<IEnumerable<BusinessPlan>> ListAsync();
     Task<BusinessPlanResponse> FindByIdAsync(int businessPlanId);
     Task<BusinessPlanResponse> SaveAsync(BusinessPlan businessPlan);
-    Task<BusinessPlanResponse> UpdateAsync(int promotionId, BusinessPlan businessPlan);
+    Task<BusinessPlanResponse> UpdateAsync(int businessPlanId, BusinessPlan businessPlan);
     Task<BusinessPlanResponse> DeleteAsync(int id);
 }
