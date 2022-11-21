6dec6daf91081266f4cc4a3ae5a8f9e829cb5034
David-Torres-V <u202010942@upc.edu.pe>
2022-11-21 16:50:11 -0500

    feat Add ordering test

---
+++ TastyPoint.API.Tests/Features/OrderService.feature
@@ -0,0 +1,16 @@
+﻿Feature: OrderServiceTests
+    As a developer
+    I want to add new Order through API
+    In order to make it available for applications.
+    
+    Background: 
+        Given the Endpoint https://localhost:7070/api/v1/orders is available
+        @order-adding
+        Scenario: Add Order With Unique Name
+            When a Post Request is sent
+            | Delivery || paymentMethod || status    | userProfileId |
+            | Rice     || cash          || Preparing | 1             |
+            Then A Response is received with Status 200
+            And a Order Resource is included in Response Body
+            | id| Delivery || paymentMethod || status    | userProfileId |
+            | 1 | Rice     || cash          || Preparing | 1             |
---
+++ TastyPoint.API.Tests/Steps/OrdersServiceStepDefinition.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Mime;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using SpecFlow.Internal.Json;
+using TastyPoint.API.Ordering.Resources;
+using TechTalk.SpecFlow.Assist;
+using Xunit;
+
+namespace TastyPoint.API.Tests.Steps;
+
+[Binding]
+public class OrdersServiceStepDefinition : WebApplicationFactory<Program>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+
+    public OrdersServiceStepDefinition(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    private HttpClient Client { get; set; }
+    private Uri BaseUri { get; set; }
+    
+    private Task<HttpResponseMessage> Response { get; set; }
+
+    [Given(@"the Endpoint https://localhost:(.*)/api/v(.*)/orders is available")]
+    public void GivenTheEndpointHttpsLocalhostApiVProductsIsAvailable(int port, int version)
+    {
+        BaseUri = new Uri($"http://localhost:{port}/api/v{version}/orders");
+        Client = _factory.CreateClient(new WebApplicationFactoryClientOptions { BaseAddress = BaseUri });
+    }
+
+    [When(@"a Post Request is sent")]
+    public void WhenAPostRequestIsSent(Table saveOrderResource)
+    {
+        var resource = saveOrderResource.CreateSet<SaveOrderResource>().First();
+        var content = new StringContent(resource.ToJson(), Encoding.UTF8, MediaTypeNames.Application.Json);
+        Response = Client.PostAsync(BaseUri, content);
+    }
+
+
+    [Then(@"A Response is received with Status (.*)")]
+    public void ThenAResponseIsReceivedWithStatus(int expectedStatus)
+    {
+        var expectedStatusCode = ((HttpStatusCode)expectedStatus).ToString();
+        var actualStatusCode = Response.Result.StatusCode.ToString();
+        
+        Assert.Equal(expectedStatusCode, actualStatusCode);
+    }
+
+    [Then(@"a Order Resource is included in Response Body")]
+    public void ThenAOrderResourceIsIncludedInResponseBody(Table orderResource)
+    {
+        var resourceExpected = orderResource.CreateSet<OrderResource>().First();
+    }
+}
